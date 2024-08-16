3558a11b510e837a45dfd5284a03e310060a3b72
SHIVA_YUYA <nikyaku405@gmail.com>
2024-08-16 14:46:36 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log(count)
+  return (
+    <>
+      <h1>Hello world</h1>
+
+      <CountPreview hoge={count} />
+      <button onClick={function(){
+        setCount(count + 1)
+        console.log(count)
+      }}>+</button>
+
+      <button onClick={function(){
+        setCount(count - 1)
+        console.log(count)
+      }}>-</button>
+    </>
+  )
+}
+
+const CountPreview = function ({hoge}:{hoge:number}) {
+  return <div>count:{hoge}</div>
+}
+
+export default App

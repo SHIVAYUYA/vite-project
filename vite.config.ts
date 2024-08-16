4721d6e68d0cc2f99aa93fe832846434f84a55fd
SHIVA_YUYA <nikyaku405@gmail.com>
2024-08-16 14:55:39 +0900

    build完了

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/vite-project/",
+  // base: process.env.GITHUB_PAGES ? "/vite-project/" : "./",
+  build: {
+    outDir: "docs",
+  },
+});
